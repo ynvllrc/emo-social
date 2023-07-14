6146269b76c58a854afa4601ebba2b92cd5c66ad
Rkenneth13 <it.russelkennethregala@gmail.com>
2023-07-14 14:57:53 +0800

    revise

--- client/src/components/Story/StoryScreen.jsx
+++ client/src/components/Story/StoryScreen.jsx
@@ -5,25 +5,105 @@ import { Typography, Box, Link } from "@mui/material";
 import useStyles from "././style";
 
 const Story = () => {
+
   const classes = useStyles();
 
   return (
     <div className={classes.body}>
-      <Box className={classes.NameTag}>
-        <div className={classes.BackButton}></div>
-        <div className={classes.HomeButton}></div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+                 }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "flex-start",
+            marginTop: "-80px",
+                
+          }}
+        >
+          <div style={{
+            marginLeft: "-40px",
+            marginTop: "-30px",
+                
+          }}
+        >
+          <img
+            src="./Back.png"
+            alt="Back"
+            style={{ height: "30%", width: "30%" }}
+            
+          />
+          
+           <img
+            src="./Home.png"
+            alt="Home"
+            
+           style={{ height: "25%", width: "30%"   }}
+          />
+        </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+           
+            marginTop: "-80px",
+            marginLeft: "90%",
+                
+          }}
+        >
 
-        <div className={classes.SettingButton}></div>
-      </Box>
-      <div className={classes.LeftButton}></div>
-
-      <div className={classes.Talking_with_friends_Button}></div>
-
-      <div className={classes.The_boy_and_his_bike_Button}></div>
-
-      <div className={classes.Playing_with_friends_Button}></div>
-
-      <div className={classes.RightButton}></div>
+          <img
+            src="./Settings.png"
+            alt="Settings"
+            style={{ height: "90%", width: "95%" }}
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            gap: "20px",
+       
+            marginTop: "10%",
+          }}
+        >
+             <img
+            src="./Prev.png"
+            alt="Prev"
+          
+            style={{ height: "10%", width: "10%", marginTop: "5%" }}
+          />
+          <img
+            src="./Talking_with_friends.png"
+            alt="Friends"
+            style={{ height: "20%", width: "20%" }}
+          />
+          <img
+            src="./The_boy_and_his_bike.png"
+            alt="Bike"
+            style={{ height: "20%", width: "20%" }}
+          />
+          <img
+            src="./Playing_with_friends.png"
+            alt="Playing"
+            style={{ height: "20%", width: "20%" }}
+          />
+             <img
+            src="./Next.png"
+            alt="Next"
+          
+            style={{ height: "10%", width: "10%", marginTop: "5%" }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
