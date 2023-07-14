01d370ed866bb96251b48afec86ff1264d663594
Laxus <ianjaspervillarico@gmail.com>
2023-07-14 12:42:55 +0800

    Explore

--- client/src/components/Explore/ExploreScreen.jsx
+++ client/src/components/Explore/ExploreScreen.jsx
@@ -8,26 +8,83 @@ const Explore = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.body}>
-      <Box className={classes.NameTag}>
-        <div className={classes.BackButton}></div>
-        <div className={classes.HomeButton}></div>
-
-        <div className={classes.SettingButton}></div>
-      </Box>
-      <div className={classes.LeftButton}></div>
-      {/* <div className={classes.Talking_with_Friend_Button}>
-         </div>  */}
-
-      <div className={classes.GuessButton}></div>
-
-      <div className={classes.BehaviorButton}></div>
-
-      <div className={classes.GoodButton}></div>
-
-      <div className={classes.RightButton}></div>
+  <div className={classes.body}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+               }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          marginTop: "-110px",
+              
+        }}
+      >
+        <img
+          src="./Back.png"
+          alt="Back"
+          style={{ height: "10%", width: "10%" }}
+        />
+         <img
+          src="./Home.png"
+          alt="Home"
+         style={{ height: "10%", width: "10%" }}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "flex-end",
+          marginTop: "-110px",
+              
+        }}
+      >  <img
+      src="./Home.png"
+      alt="Home"
+     style={{ height: "10%", width: "10%",visibility:"hidden" }}
+    />
+        <img
+          src="./Settings.png"
+          alt="Settings"
+          style={{ height: "10%", width: "15%" }}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          gap: "50px",
+          margin:"5Spx",
+        }}
+      >
+        <img
+          src="./Guess1.png"
+          alt="Story"
+          style={{ height: "20%", width: "20%" }}
+        />
+        <img
+          src="./Behavior1.png"
+          alt="Story"
+          style={{ height: "20%", width: "20%" }}
+        />
+        <img
+          src="./Good1.png"
+          alt="Story"
+          style={{ height: "20%", width: "20%" }}
+        />
+      </div>
     </div>
-  );
+  </div>
+);
 };
+
 
 export default Explore;
